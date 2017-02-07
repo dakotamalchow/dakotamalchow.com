f274dc52e80466c9a0f16a817af6abb7be50eeae
Dakota Malchow <dakotamalchow@gmail.com>
2017-02-06 20:25:14 -0500

    Redesign layout wider for cleaner look

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function loadContent(page) {
     var iframe1 = "<iframe src=";
     var iframe2 = "></iframe>";
-    document.getElementById("screen_in").innerHTML = iframe1 + page + iframe2;
+    document.getElementById("screen").innerHTML = iframe1 + page + iframe2;
 }
